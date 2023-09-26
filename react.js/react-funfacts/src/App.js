0800ff9383ff5b915f3b4de4ac9cc3bbc66c34e7
oliverjhernandez <oliverjhernandez@gmail.com>
2023-09-26 17:39:02 -0400

    Added funfacts project

---
+++ react.js/react-funfacts/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from './components/Main'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ react.js/react-funfacts/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <img
+        className="nav--icon"
+        src="react-logo.png"
+        alt="logo"
+      />
+      <h3 className="nav--logo_text">React Facts</h3>
+      <h4 className="nav--logo_title">React Course - Project 1</h4>
+    </nav>
+  )
+}
+
+export default Navbar
