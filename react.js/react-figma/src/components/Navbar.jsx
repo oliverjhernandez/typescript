a50dab7801e6994ad1ccaf345bb0761aea0f5bdb
oliverjhernandez <oliverjhernandez@gmail.com>
2023-10-24 12:39:52 -0500

    Added react-figma

---
+++ react.js/react-figma/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return <nav>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="react-logo" className='nav--icon'/>
+    <h3 className='nav--logoText'>ReactFacts</h3>
+    <h4 className='nav--title'>React Course - Project 1</h4>
+  </nav>
+}
+
+export default Navbar
